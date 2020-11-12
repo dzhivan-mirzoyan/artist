392ddc3b92670d0cf9381b910bf4861bffc66020
dzhivan-mirzoyan <63540623+dzhivan-mirzoyan@users.noreply.github.com>
2020-11-12 01:00:30 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon('icon-flag-es', sanitizer.bypassSecurityTrustResourceUrl('assets/icon/flag/es.svg'));
+    iconRegistry.addSvgIcon('icon-flag-ru', sanitizer.bypassSecurityTrustResourceUrl('assets/icon/flag/ru.svg'));
+    iconRegistry.addSvgIcon('icon-flag-gb', sanitizer.bypassSecurityTrustResourceUrl('assets/icon/flag/gb.svg'));
+  }
   title = 'artist';
-  assetsImages = ['../../assets/img/1.jpg', '../../assets/img/2.jpg', '../../assets/img/3.jpg'];
+  assetsImages = ['assets/img/1.jpg', 'assets/img/2.jpg', 'assets/img/3.jpg'];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule} from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,9 +19,11 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatIconModule,
     MatTabsModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
